af5cf2def5767c4448709cbf0d820255df39af76
BenLarry <4muki4@gmail.com>
2025-12-02 13:27:26 +0200

    mockdata and start of js

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,30 @@
 const login = document.querySelector('#login');
 const createAccount = document.querySelector('#create-account');
 
-async function accountLogin(name) {
+async function accountLogin(event) {
+  event.preventDefault();
+  const name = document.querySelector('#login-username');
+  if (name.value.includes('#')) {
+    [username, id] = name.value.split('#');
+    console.log(username, id);
+    try {
+      const response = await fetch('127.0.0.1:3000');
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+    } catch (error) {}
+  }
   // check if name is in correct format and if not return error message in window
   // if correct format fetch to server if user exist
   // if user exist object len is not 0
+  // set localstorage player id
 }
 
 async function createAccountLogin(name) {
   //remove eventlistener to login
   //limit how short and long name can be
   //create player and return name#id for player so he can use it in future
+  // set localstorage player id
 }
 
 login.addEventListener('submit', accountLogin);
