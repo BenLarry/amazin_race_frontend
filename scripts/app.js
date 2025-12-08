8746d3e5e77e2ba984140cf515d3b05e13c289b7
BenLarry <51075366+BenLarry@users.noreply.github.com>
2025-12-08 16:56:57 +0200

    Html structure js (#4)
    * logical cahnge and highscore mock data
    
    * dialog jotka näkyvät dynaamisesti
    
    * dialogs for every box
    
    * new start

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,5 @@
-const mainWindow = document.querySelector('#window');
-const loginForm = document.querySelector('#login');
+const highscoreList = document.querySelector('#highscore-list');
 
-function checkIfUserLoggedIn() {
-  const username = localStorage.getItem('username');
-  const usernameID = localStorage.getItem('usernameID');
-  if (username === null || usernameID === null) {
-    return false;
-  }
-  return true;
+async function fillHighscoreList() {
+  // create highscore list dynamicly
 }
-
-async function accountLogin(event) {
-  event.preventDefault();
-  const name = document.querySelector('#login-username');
-  if (name.value.includes('#')) {
-    [username, id] = name.value.split('#');
-    console.log(username, id);
-    localStorage.setItem('username', username);
-    localStorage.setItem('usernameID', id);
-
-    loginForm.removeEventListener('submit', accountLogin);
-
-    //remove login form and render gamewindow
-
-    /*try {
-      const response = await fetch('127.0.0.1:3000');
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-    } catch (error) {}*/
-  } else {
-    console.log('Invalid usernameform');
-  }
-}
-
-let isUserLoggedIn = checkIfUserLoggedIn();
-
-if (isUserLoggedIn) {
-  loginForm.close();
-  // IF USER IS LOGGED IN RENDER GAME WINDOW
-} else {
-  loginForm.show();
-}
-
-loginForm.addEventListener('submit', accountLogin);
