05cc0efe3b27395aedc45f4d4463abaa53cdfc5c
BenLarry <4muki4@gmail.com>
2025-12-09 14:02:41 +0200

    .

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,10 @@ const loginButton = document.querySelector('#login-btn');
 const createAccountButton = document.querySelector('#create-account-btn');
 const loginDialog = document.querySelector('#login');
 const createAccountDialog = document.querySelector('#create-account');
-<<<<<<< HEAD
 const errorMessage = document.querySelector('#error-message');
 const gameMenuDialog = document.querySelector('#game-menu');
 const gameDialog = document.querySelector('#game');
 const loginMenu = document.querySelector('#login-menu');
-=======
->>>>>>> 087a357442fca9262f3c0733012ef6b1c8858288
 
 async function fetchHighscoreList() {
   try {
@@ -20,6 +17,7 @@ async function fetchHighscoreList() {
       throw new Error('could not fetch highscore list');
     }
     const result = await response.json();
+    console.log(result);
     return result;
   } catch (error) {
     console.log(error);
@@ -29,11 +27,11 @@ async function fetchHighscoreList() {
 async function fillHighscoreList() {
   const highscore = document.querySelector('#highscore-list');
   const players = await fetchHighscoreList();
-  for (let i = 1; i < players.length; i++) {
+  for (let i = 0; i < players.length; i++) {
     console.log(players[i]);
     const li = document.createElement('li');
     li.classList.add('list-item');
-    li.innerText = `${i} ${players[i].name} ${players[i].top_points}`;
+    li.innerText = `${i + 1} ${players[i].name} ${players[i].top_points}`;
     highscore.append(li);
   }
 }
@@ -42,20 +40,14 @@ fillHighscoreList();
 
 function loginClick(event) {
   event.preventDefault();
-<<<<<<< HEAD
   loginMenu.classList.add('hide-element');
-=======
->>>>>>> 087a357442fca9262f3c0733012ef6b1c8858288
   console.log('login button clicked');
   loginDialog.showModal();
 }
 
 function createAccountClick(event) {
   event.preventDefault();
-<<<<<<< HEAD
   loginMenu.classList.add('hide-element');
-=======
->>>>>>> 087a357442fca9262f3c0733012ef6b1c8858288
   console.log('Create account button clicked');
   createAccountDialog.showModal();
 }
@@ -64,7 +56,6 @@ async function login(event) {
   event.preventDefault();
   console.log('Login submit clicked');
 
-<<<<<<< HEAD
   const usernameInput = document.querySelector('#username-login').value;
   if (!usernameInput.includes('#')) {
     return (errorMessage.innerText = 'Invalid username format, username#ID');
@@ -93,24 +84,11 @@ async function login(event) {
   } catch (error) {
     console.log(error);
   }
-=======
-  const username = document.querySelector('#username-login').value;
-  if (!username.includes('#')) {
-  }
-  const x = 'perk';
-  const y = 72;
-  try {
-    const response = fetch(
-      `http://127.0.0.1:3000/login?name=${x}&player_ID=${y}`
-    );
-  } catch (error) {}
->>>>>>> 087a357442fca9262f3c0733012ef6b1c8858288
 }
 
 async function createAccount(event) {
   event.preventDefault();
   console.log('Create account submit clicked');
-<<<<<<< HEAD
   const usernameInput = document.querySelector(
     '#username-create-account'
   ).value;
@@ -134,10 +112,6 @@ async function createAccount(event) {
   } catch (error) {
     console.log(error);
   }
-=======
-  try {
-  } catch (error) {}
->>>>>>> 087a357442fca9262f3c0733012ef6b1c8858288
 }
 
 loginButton.addEventListener('click', loginClick);
