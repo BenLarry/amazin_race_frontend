dea3c7624551305a9d626cac7682267731f22cd5
BenLarry <4muki4@gmail.com>
2025-12-09 10:04:17 +0200

    highscore

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,29 @@
-const highscoreList = document.querySelector('#highscore-list');
+'use strict';
+
+async function fetchHighscoreList() {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/highscore');
+
+    if (!response.ok) {
+      throw new Error('could not fetch highscore list');
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function fillHighscoreList() {
-  // create highscore list dynamicly
+  const highscore = document.querySelector('#highscore-list');
+  const players = await fetchHighscoreList();
+  for (let i = 1; i < players.length; i++) {
+    console.log(players[i]);
+    const li = document.createElement('li');
+    li.classList.add('list-item');
+    li.innerText = `${i} ${players[i].name} ${players[i].top_points}`;
+    highscore.append(li);
+  }
 }
+
+fillHighscoreList();
