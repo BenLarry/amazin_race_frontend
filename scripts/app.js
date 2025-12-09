5819d564c16ada2749f58f0f16eb7880d9bf5bd0
BenLarry <4muki4@gmail.com>
2025-12-09 11:55:34 +0200

    login

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const loginButton = document.querySelector('#login-btn');
+const createAccountButton = document.querySelector('#create-account-btn');
+const loginDialog = document.querySelector('#login');
+const createAccountDialog = document.querySelector('#create-account');
+
 async function fetchHighscoreList() {
   try {
     const response = await fetch('http://127.0.0.1:3000/highscore');
@@ -27,3 +32,44 @@ async function fillHighscoreList() {
 }
 
 fillHighscoreList();
+
+function loginClick(event) {
+  event.preventDefault();
+  console.log('login button clicked');
+  loginDialog.showModal();
+}
+
+function createAccountClick(event) {
+  event.preventDefault();
+  console.log('Create account button clicked');
+  createAccountDialog.showModal();
+}
+
+async function login(event) {
+  event.preventDefault();
+  console.log('Login submit clicked');
+
+  const username = document.querySelector('#username-login').value;
+  if (!username.includes('#')) {
+  }
+  const x = 'perk';
+  const y = 72;
+  try {
+    const response = fetch(
+      `http://127.0.0.1:3000/login?name=${x}&player_ID=${y}`
+    );
+  } catch (error) {}
+}
+
+async function createAccount(event) {
+  event.preventDefault();
+  console.log('Create account submit clicked');
+  try {
+  } catch (error) {}
+}
+
+loginButton.addEventListener('click', loginClick);
+createAccountButton.addEventListener('click', createAccountClick);
+
+loginDialog.addEventListener('submit', login);
+createAccountDialog.addEventListener('submit', createAccount);
