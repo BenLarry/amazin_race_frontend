b0fd8459448cb227fbaf7b33ab8770c82aa140d0
Nikolas <nikolas.auvinen@gmail.com>
2025-12-10 18:08:16 +0200

    Ulkonäköa about valmis

--- scripts/app.js
+++ scripts/app.js
@@ -100,8 +100,7 @@ async function login(event) {
 
     await fillOldGameList();
     loginDialog.close();
-    welcomeText.innerText = `Hei, ${localStorage.getItem('username')}
-    #${localStorage.getItem('ID')}`;
+    welcomeText.innerText = `Hei, ${localStorage.getItem('username')}#${localStorage.getItem('ID')}`;
 
     gameMenuDialog.showModal();
   } catch (error) {
